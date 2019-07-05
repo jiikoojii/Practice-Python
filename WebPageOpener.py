deeeed454d3a366801571d4ab5803b1d9d392ab4
Jani Järvinen <jani.jarvinen@qvantel.com>
2019-07-05 09:39:03 +0300

    Just a little script, might save a couple of seconds

---
+++ WebPageOpener.py
@@ -0,0 +1,7 @@
+import webbrowser
+webbrowser.open('https://ilmatieteenlaitos.fi/saa/jyv%C3%A4skyl%C3%A4')
+webbrowser.open('https://iltalehti.fi')
+webbrowser.open('https://twitter.com')
+webbrowser.open('https://discordapp.com')
+
+#Just to open up some sites when starting workday
